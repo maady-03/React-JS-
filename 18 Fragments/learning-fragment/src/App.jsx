31205da7ea1379b56a3dc02df2ee9e4a9644ab89
Manas Verma <manasverma1269@gmail.com>
2025-12-15 10:19:32 +0000

    Learned about Passing children in props

--- 18 Fragments/learning-fragment/src/App.jsx
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -1,19 +1,28 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import FoodItems from './components/FoodItems';
-import ErrorMessage from './components/ErrorMessage'; 
-import './App.css';
+import FoodItems from "./components/FoodItems";
+import ErrorMessage from "./components/ErrorMessage";
+import "./App.css";
+import Container from "./components/Container";
 
 function App() {
-
   let foodItems = ["Dal", "Green Vegetables", "roti", "salad", "Milk", "Ghee"];
 
   return (
-    <div>
-      <h1 className="food-heading">Healthy Food</h1>
-      <ErrorMessage items={foodItems} />
-      <FoodItems items={foodItems} />
-    </div>
-  ); 
+    <>
+      <Container>
+        <h1 className="food-heading">Healthy Food</h1>
+        <ErrorMessage items={foodItems} />
+        <FoodItems items={foodItems} />
+      </Container>
+
+      <Container>
+        <p>
+          Above is the list of the healthy foods that are good
+          for your heakth and well being.
+        </p>
+      </Container>
+    </>
+  );
 }
 
 export default App;
--- 18 Fragments/learning-fragment/src/components/Item.jsx
+++ 18 Fragments/learning-fragment/src/components/Item.jsx
@@ -1,7 +1,7 @@
 import  styles from"./Item.module.css"
 const Item = ({ foodItem }) => {
   return (
-    <li className={`${styles["Kg-item"]}`}>
+    <li className={`${styles["Kg-item"]} list-group-item`}>
       <span className={styles["kg-span"]}>{foodItem}</span></li>
   );
 };
